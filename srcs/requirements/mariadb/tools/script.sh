d1d92bab3c098e755c7d61d1f61602510844bd6d
sm222 <soleil.maraska@gmail.com>
2024-05-09 18:34:33 +0000

    edit marriadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,12 +4,17 @@ chown -R mysql:mysql /var/lib/mysql
 
 mysql_install_db --datair=/var/lib/mysql --user=mysql --skip-test-db >> /dev/null
 
+OUTFILE='tmpfile.sql'
 
 
+echo "FLUSH PRIVILEGES;" > $OUTFILE
+echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" >> $OUTFILE
+echo "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> $OUTFILE
+echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';" >> $OUTFILE
+echo "ALTER USER \`root\`@\`localhost\` IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> $OUTFILE
+echo "GRANT ALL PRIVILAGE ON *.* TO 'root'@'localhost' WITH GRANT OPTION;" >> $OUTFILE
+echo "FLUSH PRIVILEGES;" >> $OUTFILE
 
+mysql --user=mysql --bootstrap < $OUTFILE
 
-echo "FLUSH PRIVILEGES;" > tmp.sql
-echo "" > tmp.sql
-echo "" > tmp.sql
-echo "" > tmp.sql
-echo "" > tmp.sql
+rm -f $OUTFILE
